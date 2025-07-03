b850c28e175ba41b7ea6f8715a667e61a5388c1c
Andrea <andreat.nava@gmail.com>
2025-07-03 10:45:24 +0200

    updated plotting for effective range

--- src/lagp/utils/analyse_results.py
+++ src/lagp/utils/analyse_results.py
@@ -535,7 +535,7 @@ def make_plots_hypers(df, configs, metrics, version):
             df_filtered = df_filtered[df_filtered["likelihood"] == likelihood]
             # Assuming 'model_method' is the column representing different methods
 
-            effective_range = lengthscale # / 2.7
+            effective_range = lengthscale * np.sqrt(dim/2) if dim > 2 else lengthscale
             true_pars["lengthscale"] = effective_range
             for metric in metrics:
                 plt.figure(figsize=(10, 6))

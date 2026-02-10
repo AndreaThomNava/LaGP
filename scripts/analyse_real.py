daeea45d973be868014b5b343219e7aea020920c
Andrea <andreat.nava@gmail.com>
2026-02-10 14:31:38 +0100

    added gitignore

--- scripts/analyse_real.py
+++ scripts/analyse_real.py
@@ -18,8 +18,8 @@ def run_analysis(RESULT_PATH, CONFIGS_PATH, OUTPUT_DIR):
         configs = yaml.safe_load(f) 
 
     metrics = [
-        ("quantile_loss_mean", "quantile_loss_std", "Quantile Loss"),  # 3rd element is the name/label
-     #   ("time_mean", "time_std", "Time")
+     #   ("quantile_loss_mean", "quantile_loss_std", "Quantile Loss"),  # 3rd element is the name/label
+       ("time_mean", "time_std", "Time")
     ]
     # Define metrics and optimality
     metric_criteria = {"Quantile Loss": "min",  "Time": "min"}

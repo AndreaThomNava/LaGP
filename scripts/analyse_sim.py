11bf2814a059861dfe797081924b3730acb83f71
Andrea <andreat.nava@gmail.com>
2026-02-27 13:48:42 +0100

    change mehtods name to TKC

--- scripts/analyse_sim.py
+++ scripts/analyse_sim.py
@@ -30,8 +30,8 @@ def run_analysis(RESULT_PATH, CONFIGS_PATH, OUTPUT_DIR):
         # ("quantile_loss_mean", "quantile_loss_std", "Quantile Loss"),  # 3rd element is the name/label
         # ("interval_loss_mean", "interval_loss_std", "Interval Score"),
         # ("coverage_mean", "coverage_std", "Coverage"),
-        ("time_mean", "time_std", "Time"),
-        # ("rmse_mean", "rmse_std", "RMSE")
+        #("time_mean", "time_std", "Time"),
+        ("rmse_mean", "rmse_std", "RMSE")
     ]
     # Define metrics and optimality
     metric_criteria = {
